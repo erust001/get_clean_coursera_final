a04175c56d4d06a18e9a6059c7158fd8d766c197
Rustamov Elvin <rustamovelvin001@gmail.com>
2020-09-12 20:59:08 +0400

    proverka

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,15 @@
-#
+library(tidyverse)
+rawDataDir <- "./rawData"
+rawDataUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+rawDataFilename <- "rawData.zip"
+rawDataDFn <- paste(rawDataDir, "/", "rawData.zip", sep = "")
+dataDir <- "./data"
+
+if (!file.exists(rawDataDir)) {
+  dir.create(rawDataDir)
+  download.file(url = rawDataUrl, destfile = rawDataDFn)
+}
+if (!file.exists(dataDir)) {
+  dir.create(dataDir)
+  unzip(zipfile = rawDataDFn, exdir = dataDir)
+}
